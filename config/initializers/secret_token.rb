1055fd061ef8d776024f546db4f35d1a0eac5ef3
Marques Jordan <marques.jordan81@gmail.com>
2014-11-17 14:45:36 -0800

    Initial Commit, Set PG database & Add Bootstrap Gem

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Gotjokes::Application.config.secret_key_base = 'c0496b3d0bce29088652a05654257b35f725d90c30e673cefac44dc1f5dfc8a4a9a887b3868026c4124d1f151f77e8095a6bdcd8d12e8179973472a3cc2d3a04'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Gotjokes::Application.config.session_store :cookie_store, key: '_gotjokes_session'
