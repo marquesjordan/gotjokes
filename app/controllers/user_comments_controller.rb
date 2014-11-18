b984b43c2cf4e50c0253711ff6b7323368f94df5
thirdchance57 <thirdchance57@hotmail.com>
2014-11-17 19:20:34 -0800

    adding new vote view and controller

--- app/controllers/user_comments_controller.rb
+++ app/controllers/user_comments_controller.rb
@@ -2,4 +2,48 @@ class UserCommentsController < ApplicationController
   def index
   end
 
+  def show
+    @user_comment = user_comment.find(params[:id])
+    # @pastries = @user_comment.pastries ;
+  end
+
+  def new
+    @user_comment = user_comment.new
+  end
+
+  def create 
+    @user_comment = user_comment.new(user_comment_params)
+    if @user_comment.save
+      redirect_to user_comments_path
+    else
+      render 'new'
+    end
+  end
+
+  def edit
+    @user_comment = user_comment.find(params[:id])
+    # 1.times { @user_comment.pastries.build }
+  end
+
+  def update
+    @user_comment = user_comment.find(params[:id])
+    if @user_comment.update_attributes(user_comment_params)
+      redirect_to user_comments_path
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @user_comment = user_comment.find(params[:id])
+    @user_comment.destroy
+    redirect_to user_comments_path
+  end
+
+  private
+  def user_comment_params
+    params.require(:user_comment).permit(
+      )
+  end
+
 end
--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,4 +1,31 @@
 class VotesController < ApplicationController
+  
   def index
+    @votes = Vote.all
   end
+
+  def show
+    # @vote = Vote.find(params[:id])
+    # # @pastries = @bean.pastries ;
+  end
+
+  def new
+    @vote = Vote.new
+  end
+
+  def create 
+    @vote = Vote.new(vote_params)
+    if @vote.save
+      redirect_to votes_path
+    else
+      render 'new'
+    end
+  end
+
+  private
+  def vote_params
+    params.require(:vote).permit(
+      :vote)
+  end
+
 end
