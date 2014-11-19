249c2a359922d2a2687ee3e450a7508211a7caf3
thirdchance57 <thirdchance57@hotmail.com>
2014-11-19 13:16:13 -0800

    fixing heroku minification angular bug

--- app/assets/javascripts/angular/app.js
+++ app/assets/javascripts/angular/app.js
@@ -1,17 +1,16 @@
 var app = angular.module('myApp', ['ui.router', 'templates'])
 
-.config(function($stateProvider, $urlRouterProvider){
+.config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $urlRouterProvider){
 
   $urlRouterProvider.otherwise('/');
 
   $stateProvider
-
     .state('home', {
       url: '/home',
       templateUrl: 'home.html',
       controller: 'homeController'
     });
-})
+}])
 
 .controller('homeController', function($scope) {
   console.log("angular console log test");
