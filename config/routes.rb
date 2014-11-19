21fbdcc33304212e4f3bf40f96b0f32f97fb7f33
zgolman <zgolman@gmail.com>
2014-11-19 14:45:02 -0800

    signup starter

--- config/routes.rb
+++ config/routes.rb
@@ -5,23 +5,27 @@ Gotjokes::Application.routes.draw do
   # post "jokes/" => 'jokes#create'
   # get "jokes/edit"
 
-  get 'jokes/index'     =>    'jokes#index',    as: :jokes
-  get 'jokes/new'       =>    'jokes#new',      as: :new_jokes
-  get "jokes/:id"       =>    'jokes#show',     as: :joke
-  post 'jokes/create'   =>    'jokes#create' 
-  get "jokes/:id/edit"  =>    'jokes#edit',     as: :edit_joke
-  patch 'jokes/:id'     =>    'jokes#update'
-  delete 'jokes/:id'    =>    'jokes#destroy',  as: :delete_joke
+  # get 'jokes/index'     =>    'jokes#index',    as: :jokes
+  # get 'jokes/new'       =>    'jokes#new',      as: :new_jokes
+  # get "jokes/:id"       =>    'jokes#show',     as: :joke
+  # post 'jokes/create'   =>    'jokes#create' 
+  # get "jokes/:id/edit"  =>    'jokes#edit',     as: :edit_joke
+  # patch 'jokers/:id'     =>    'jokes#update'
+  # delete 'jokes/:id'    =>    'jokes#destroy',  as: :delete_joke
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
   
+  resources :users 
+
+  resources :jokes
+
   resources :profiles
 
   resources :votes
 
   resources :user_comments
 
- 
+  
 
 
 end
